30978783330fc2287db3aef85e75b930e48236dc
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-24 11:21:25 -0300

    add page

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <h2>Welcome to Next.js!</h2>
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default HomePage
