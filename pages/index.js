7469c598845d03b3458342baef84e28518d52552
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-01-24 11:46:23 -0300

    add GlobalStyle

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,38 @@
+function GlobalStyle() {
+  return (
+    <style global jsx>{`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+        background-color: teal;
+      }
+
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+
+      #__next {
+        flex: 1;
+      }
+
+      #__next > * {
+        flex: 1;
+      }
+
+      /* ./App fit Height */ 
+    `}</style>
+  )
+}
+
 function Title(props) {
   const Tag = props.tag
   return (
@@ -15,6 +50,7 @@ function Title(props) {
 function HomePage() {
   return (
     <div>
+      <GlobalStyle />
       <Title tag="h2">Welcome to Next.js! Title</Title>
       <h2>Welcome to Next.js! H2</h2>
     </div>
